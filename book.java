f07745a1026e7344e3367dad0a5c85cccaf1164f
Kaviya-0006 <kaviya63690@gmail.com>
2026-02-04 15:54:27 +0530

    Commit of second programm

---
+++ book.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Library {
+    public static void main(String[] args) {
+        ArrayList<String> books = new ArrayList<>();
+
+        books.add("Python");
+        books.add("DBMS");
+
+        System.out.println("Books in library: " + books);
+    }
+}
